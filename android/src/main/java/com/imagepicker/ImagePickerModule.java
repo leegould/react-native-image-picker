0ec3c2833bdebc3c8ee4fffac9ef96f90ade1122
Lee Gould <gould.lee@googlemail.com>
2017-12-21 15:20:55 +0000

    merging some PR's

--- android/src/main/java/com/imagepicker/ImagePickerModule.java
+++ android/src/main/java/com/imagepicker/ImagePickerModule.java
@@ -365,7 +365,7 @@ public class ImagePickerModule extends ReactContextBaseJavaModule
     responseHelper.cleanResponse();
 
     // user cancel
-    if (resultCode != Activity.RESULT_OK)
+    if (requestCode != REQUEST_LAUNCH_IMAGE_CAPTURE && resultCode != Activity.RESULT_OK)
     {
       removeUselessFiles(requestCode, imageConfig);
       responseHelper.invokeCancel(callback);
@@ -436,13 +436,22 @@ public class ImagePickerModule extends ReactContextBaseJavaModule
     BitmapFactory.Options options = new BitmapFactory.Options();
     options.inJustDecodeBounds = true;
     BitmapFactory.decodeFile(imageConfig.original.getAbsolutePath(), options);
+
+    if (options.outMimeType == null || options.outWidth < 1)
+    {
+        removeUselessFiles(requestCode, imageConfig);
+        responseHelper.invokeCancel(callback);
+        callback = null;
+        return;
+    }
+
     int initialWidth = options.outWidth;
     int initialHeight = options.outHeight;
-    updatedResultResponse(uri, imageConfig.original.getAbsolutePath());
 
     // don't create a new file if contraint are respected
     if (imageConfig.useOriginal(initialWidth, initialHeight, result.currentRotation))
     {
+      updatedResultResponse(uri, imageConfig.original.getAbsolutePath());
       responseHelper.putInt("width", initialWidth);
       responseHelper.putInt("height", initialHeight);
       fileScan(reactContext, imageConfig.original.getAbsolutePath());
